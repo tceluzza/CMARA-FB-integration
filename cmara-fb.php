b332e7453507673d12e8d3bd1070920f619feb89
Thomas Celuzza <tomceluzza@gmail.com>
2025-01-02 14:26:31 -0500

    start plugin

---
+++ cmara-fb.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: CMARA Facebook Integration
+ * Plugin URI: https://www.cmara.org/
+ * Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin elit non molestie tempor. Nam rhoncus eleifend libero in pulvinar. Integer scelerisque diam vitae cursus ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed aliquam congue neque vel dignissim. Phasellus auctor tincidunt purus et dictum. Suspendisse quis velit dolor. In commodo consequat metus sed aliquet. Donec mollis odio nec massa dapibus, sed cursus est cursus.
+ * Author: Thomas Celuzza
+ * Version: 0.01
+ * License: GPL v3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+?>
