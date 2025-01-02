2df2b9788a071d1aa63e2788afa500cdba85c1fa
Thomas Celuzza <tomceluzza@gmail.com>
2025-01-02 15:59:08 -0500

    Add a field without knowing what I'm doing

--- cmara-fb.php
+++ cmara-fb.php
@@ -4,6 +4,7 @@
  * Plugin URI: https://www.cmara.org/
  * Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin elit non molestie tempor. Nam rhoncus eleifend libero in pulvinar. Integer scelerisque diam vitae cursus ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed aliquam congue neque vel dignissim. Phasellus auctor tincidunt purus et dictum. Suspendisse quis velit dolor. In commodo consequat metus sed aliquet. Donec mollis odio nec massa dapibus, sed cursus est cursus.
  * Author: Thomas Celuzza
+ * Author URI:  https://github.com/tceluzza/CMARA-FB-integration
  * Version: 0.01
  * License: GPL v3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
@@ -12,6 +13,11 @@
 /**
  * Activate the plugin.
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+  die( 'Invalid request.' );
+}
+
 function cmarafb_activate() { 
 
 }
@@ -28,6 +34,7 @@ register_deactivation_hook( __FILE__, 'cmarafb_deactivate' );
 /**
  * Options page for the plugin
  */
+
 function cmarafb_options_page_html() {
   // check user capabilities
   if ( ! current_user_can( 'manage_options' ) ) {
@@ -51,17 +58,73 @@ function cmarafb_options_page_html() {
   <?php
 }
 
-function cmarafb_options_page() {
-  add_submenu_page(
-    'options-general.php',
-    'CMARA FB',
-    'CMARA FB Options',
-    'manage_options',
-    'cmara_fb',
-    'cmarafb_options_page_html'
+function cmarafb_settings_init() {
+  register_setting(
+    'options',
+    'cmara_pageid'
   );
+
+  add_settings_section(
+		'cmarafb_settings_section',
+		'CMARA Facebook Settings',
+    'cmarafb_settings_callback',
+		'options'
+	);
+
+  add_settings_field(
+		'cmarafb_pageid_field',
+		'Facebook Page ID',
+    'cmarafb_pageid_callback',
+		'options',
+		'cmarafb_settings_section'
+	);
 }
-add_action( 'admin_menu', 'cmarafb_options_page' );
+
+/**
+ * register wporg_settings_init to the admin_init action hook
+ */
+add_action('admin_init', 'cmarafb_settings_init');
+
+/**
+ * callback functions
+ */
+
+// section content cb
+function cmarafb_settings_callback() {
+	echo '<p>CMARA FB Introduction.</p>';
+}
+
+// field content cb
+function cmarafb_pageid_callback() {
+	// get the value of the setting we've registered with register_setting()
+	$setting = get_option('cmara_pageid');
+	// output the field
+	?>
+	<input type="text" name="cmara_pageid" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+    <?php
+}
+
+
+
+
+
+
+
+
+
+
+// function cmarafb_options_page() {
+//   add_submenu_page(
+//     'options-general.php',
+//     'CMARA Facebook Integration',
+//     'CMARA FB Options',
+//     'manage_options',
+//     'cmara_fb',
+//     'cmarafb_options_page_html',
+//     20
+//   );
+// }
+// add_action( 'admin_menu', 'cmarafb_options_page' );
 
 
 ?>
