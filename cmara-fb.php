84352b38e449e99bcbedd8d3a12c4e20249b0fd0
Thomas Celuzza <tomceluzza@gmail.com>
2025-01-05 16:53:58 -0500

    Don't attempt to post if no access has been given. Also, update version

--- cmara-fb.php
+++ cmara-fb.php
@@ -2,16 +2,12 @@
 /**
  * Plugin Name: CMARA Facebook Integration
  * Plugin URI: https://www.cmara.org/
- * Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin elit non molestie tempor. Nam rhoncus eleifend libero in pulvinar. Integer scelerisque diam vitae cursus ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed aliquam congue neque vel dignissim. Phasellus auctor tincidunt purus et dictum. Suspendisse quis velit dolor. In commodo consequat metus sed aliquet. Donec mollis odio nec massa dapibus, sed cursus est cursus.
+ * Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sollicitudin elit non molestie tempor. Nam rhoncus eleifend libero in pulvinar.
  * Author: Thomas Celuzza
  * Author URI:  https://github.com/tceluzza/CMARA-FB-integration
- * Version: 0.01
+ * Version: 1.01
  * License: GPL v3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
- */
-
-/**
- * Activate the plugin.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -240,6 +236,10 @@ function post_to_facebook( $new_status, $old_status, $post ) {
 	$db_data = get_option( 'cmarafb_option_name' ); // Array of All Options
 	$page_id = $encryption->decrypt( $db_data['page_id_0'] ); // Page ID
 	$page_access_token = $encryption->decrypt( $db_data['page_access_token_1'] ); // Page Access Token
+
+	if ( '' === $page_id || '' === $page_access_token ) {
+		return false;
+	}
 
   // Normal things: where we want to post
   $fb_api = 'https://graph.facebook.com/';
